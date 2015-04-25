40d51cc297e726b58ad8deed624f988f61a7c652
BT <bernardotolosajr@gmail.com>
2015-04-25 11:24:15 +0800

    initial commit

---
+++ test/index.js
@@ -0,0 +1,55 @@
+var expect = require('chai').expect,
+    sinon = require('sinon'),
+    needle = require('needle'),
+    SMS = require('../lib/sms');
+
+describe('SMS', function(){
+  var mock = null;
+
+  beforeEach(function() {
+    mock = sinon.mock(needle);
+  });
+
+  afterEach(function() {
+    mock.restore();
+  });
+
+  it('should be object', function() {
+    var sms = SMS();
+    expect(sms).to.be.ok;
+  });
+
+  it('should throw if no options is passed in', function() {
+    var sms = SMS();
+    expect(function() {
+        sms.send({}, sinon.spy());
+      }).to.throw('missing options');
+  });
+
+  describe('#send', function() {
+    it('should call the needle.post', function() {
+      var url = 'https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/5315/requests?access_token=1234';
+
+      var data = {
+        address: '09150000000',
+        message: 'hello'
+      };
+
+      mock.expects('post')
+        .once()
+        .withArgs(url, data);
+
+      var sms = SMS({senderAddress: '5315', token: '1234'})
+      sms.send(data, sinon.spy());
+
+      mock.verify();
+    });
+
+    it('should throw if no data is passed in', function() {
+      var sms = SMS({senderaddress: '5315', token: '1234'})
+      expect(function() {
+          sms.send({}, sinon.spy());
+        }).to.throw('missing data');
+    });
+  });
+})
